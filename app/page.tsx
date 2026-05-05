cd51c8e7f36f5aa8d8e671e875df770a9fd44aa4
christinesofiamartinez-hub <christinesofiamartinez@gmail.com>
2026-05-06 01:53:27 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -478,7 +478,7 @@ export default function Home() {
         <div className="faq-inner">
   <div className="faq-left fade-up">
     <h2>Questions worth asking.</h2>
-    <p>If you don&apos;t see your question here...</p>
+    <p>If you don&apos;t see your question here, email us at hello@calar.me and we&apos;ll answer honestly.</p>
   </div>
   <div className="faq-list fade-up d1">
             {faqs.map(({ q, a }, i) => (
